df33f9576113ecc05dd8d51e9cdad6032a02f314
Naoto Matsumoto <s1F102001243@iniad.org>
2022-09-19 04:27:35 +0900

    UPDATED src/content.js: 用語をボタンに置き換える機能

--- src/content.js
+++ src/content.js
@@ -1,8 +1,10 @@
 // 用語リスト(暫定)
 var word_list = ["CPU","RAM"];
 
+// ボタンのidようにボタンの数を数え上げる
+var button_num = 0;
 
-// injectしたいコードを一つの関数にまとめる
+// ボタン押下時に実行される関数
 function OnClickLink(word) {
     alert(word);
 }
@@ -19,16 +21,18 @@ for (let i=0; i<paragraphs.length; i++) {
     
     // 単語一つ一つに対して処理
     for (let j=0; j<word_list.length; j++) {
-         tmp = paragraph.innerHTML.replace(word_list[j], `<button type="button" class='easy-term-auto'>${word_list[j]}</button>`);
-         if (tmp !== paragraph) {
+        // paragraph内の用語をボタンに置換したものをtmpに代入する
+         tmp = paragraph.innerHTML.replace(word_list[j], `<a type="button" id='easy-term-auto-${button_num}'>${word_list[j]}</a>`);
+         // paragraphとtmpが異なればparagraphのinnerHTMLをtmpに置換
+         if (tmp !== paragraph.innerHTML) {
             paragraph.innerHTML = tmp;
+            // button_num数え上げ
+            button_num += 1;
          }
     }
 
 } //paragraphs[i]の処理終わり
-
-var elements = document.getElementsByClassName("easy-term-auto");
-for (let i=0; i<elements.length; i++) {
-    var element = elements[i];
-    element.addEventListener("click", function () {OnClickLink(element.innerText)});
+for (let i=0; i<button_num; i++) {
+    const button = document.getElementById(`easy-term-auto-${i}`);
+    button.addEventListener('click', function () {OnClickLink(button.innerText)});
 }
