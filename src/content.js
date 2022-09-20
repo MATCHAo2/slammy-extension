b6509a9afae7c37b19b8f52f2166003cf62e7962
Naoto Matsumoto <s1F102001243@iniad.org>
2022-09-21 01:16:14 +0900

    UPDATE content.js: ポップアップ(というよりツールチップ？）を表示させる関数を作成

--- src/content.js
+++ src/content.js
@@ -12,11 +12,13 @@ const popup = document.createElement('div');
 popup.setAttribute('class', 'easy-term-popup');
 document.body.appendChild(popup);
 
-// ボタン押下時に実行される関数
-function OnClickLink(word) {
-    alert(word);
+// ポップアップを表示させる関数
+function () renderPopup(mouseX, mouseY, selection) {
+    popup.innerHTML = selection;
+    popup.style.top = mouseY + 'px';
+    popup.style.left = mouseX + 'px';
+    popup.style.visibility = 'visible';
 }
-
 
 // webページの<p>要素のリスト
 const paragraphs = document.querySelectorAll("p");
@@ -40,7 +42,20 @@ for (let i=0; i<paragraphs.length; i++) {
     }
 
 } //paragraphs[i]の処理終わり
+
+// ボタンクリックを監視し、クリック時の動作を決める
 for (let i=0; i<button_num; i++) {
     const button = document.getElementById(`easy-term-auto-${i}`);
-    button.addEventListener('click', function () {OnClickLink(button.innerText)});
+    button.addEventListener('click', function (event) {
+        if (popup.style.visibility !== 'hidden') {
+            // popupが表示されている場合、クリックしたときに非表示にする
+            popup.style.visibility === 'hidden';
+        } else {
+            // popupが表示されていない場合、クリックしたときにカーソルの近くに表示するようにする
+            const selection = window.getSelection().toString();
+            if (selection.length > 0) {
+                renderPopup(event.clientX, event.clientY, selection);
+            }
+        }
+    });
 }
