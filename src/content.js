460f2e2262dea93ec6423f6c3e5b82732f6de94a
Naoto Matsumoto <s1F102001243@iniad.org>
2022-12-07 15:13:57 +0900

    整理

--- src/content.js
+++ src/content.js
@@ -98,11 +98,6 @@ function buttonBehavior(popup, button_num, word_list) {
 let popup = document.createElement('div');
 popup.setAttribute('class', 'easy-term-popup');
 document.body.appendChild(popup);
-// ポップアップの詳細ボタン生成
-let extend_btn = document.createElement("a");
-extend_btn.innerText = "詳細";
-extend_btn.id = "easy-term-popup-extend_btn";
-popup.appendChild(extend_btn);
 
 // APIから用語リスト取得
 fetch(apiUrl + "words",{method: "GET"})
