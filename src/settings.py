3b966ce3b33a6ad6bf2f5070f43a42219e270dfe
Naoto Matsumoto <s1F102001243@iniad.org>
2023-01-05 23:52:08 +0900

    設定用HTMLの原型

---
+++ src/settings.py
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+
+        <meta charset="UTF-8">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;500&display=swap" rel="stylesheet">
+
+
+        <title>Easy-Term</title>
+
+        <style>
+            body {
+                
+                width: 250px;
+                height: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>popup.html</h1>
+    </body>
+</html>
