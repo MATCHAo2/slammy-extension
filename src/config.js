0bbc2021813fc1b4d7aaa7fc81aab322bfd70a31
Naoto Matsumoto <s1F102001243@iniad.org>
2023-01-06 00:24:10 +0900

    chromeのローカルストレージにslammyの設定を保持するプログラムを書きました

---
+++ src/config.js
@@ -0,0 +1,23 @@
+// ~~~~~関数定義~~~~~ //
+// 保存ボタンを押されたときにchromeのlocal storageに設定項目を保存
+function save_configs() {
+    let isUse = document.getElementById('easy-term-config-is-use').checked;
+    chrome.storage.local.set({
+        slammyIsUse: isUse
+    }, function() {
+        let message = document.getElementById('easy-term-config-message');
+        message.innerText = '保存しました!';
+    });
+}
+
+// 設定を開いた時に、設定項目の値をHTMLに反映させる関数
+function restore_configs() {
+    chrome.storage.local.get("slammyIsUse", function(value) {
+        document.getElementById('easy-term-config-is-use').checked = value.slammyIsUse;
+    });
+}
+
+// ~~~~~関数定義 終~~~~~//
+
+document.addEventListener('DOMContentLoaded', restore_configs);
+document.getElementById('easy-term-config-save').addEventListener('click', save_configs);
