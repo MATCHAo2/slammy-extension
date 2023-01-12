844baa802ca6d58ca74c05187723504d6f071c9b
Naoto Matsumoto <s1F102001243@iniad.org>
2023-01-12 15:30:40 +0900

    関数の定義を別ファイルに分割しました

--- src/content.js
+++ src/content.js
@@ -31,97 +31,6 @@ let popupDesc, popupDetailDesc, popupImgDesc;
 // ~~~~~~~~~~~~変数定義 終~~~~~~~~~~~ //
 
 
-// ~~~~~~~~~~~~関数の定義~~~~~~~~~~~~ //
-// 引数に与えられた文字列が日本語であるかどうかを判定する関数
-function ja2Bit ( str ) {
-  return ( str.match(/^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/) )? true : false;
-}
-
-
-// ページ内にボタンを配置する関数
-function setButtonsInPage(paragraphs, word) {
-    // paragraphsに入っているノードごとに処理
-    for (i=0; i<paragraphs.length; i++) {
-        // paragraphはparagraphsの上からi番目の要素
-        paragraph = paragraphs[i];
-
-        // paragraphの子ノードを一つずつ処理
-        for (let j=0; j<paragraph.childNodes.length; j++) {
-            // childはparagraphの子ノード
-            child = paragraph.childNodes[j];
-            // textはparagraphのテキスト
-            text = child.textContent;
-            // whereは単語がtextの中で出てくる位置
-            where = text.indexOf(word);
-            // newNodesを空にしておく
-            newNodes = [];
-            if(where !== -1) {
-                // textに用語が含まれていれば新しいテキストノードとaタグの子要素を作成する
-                // textを用語以前と用語より後に分割し、それぞれnewTextとnewText2に代入する
-                newText = text.substr(0, where+word.length);
-                newText2 = text.substr(where+word.length);
-                // newText2の1文字目が日本語でない場合、ここより下の行は実行しない
-                let first_char = newText2.charAt(1);
-                let last_char = newText.charAt(newText.length);
-                if (!ja2Bit(first_char) && !ja2Bit(last_char)) {
-                    continue;
-                }
-                // aタグのノードを作成する
-                a = document.createElement('a');
-                a.text = '解説';
-                a.id = `easy-term-auto-${buttonNum}`;
-                a.name = word;
-                a.style.fontSize = '30%';
-                // 新しいノードをnewNodesにまとめておく
-                newNodes.push(document.createTextNode(newText)) 
-                newNodes.push(a);
-                newNodes.push(document.createTextNode(newText2));
-
-                // paragraphの新しいchildと置き換える古いchildを削除
-                paragraph.removeChild(child);
-
-                // paragraphに新しいテキストノードを挿入する
-                for (let x=newNodes.length-1; x>=0; x--) {
-                    paragraph.insertBefore(newNodes[x], paragraph.childNodes[j]);
-                }
-
-                // HTMLタグのid用の変数を数え上げ
-                buttonNum++;
-                // 新しい要素が2つ追加されているので、jを2繰り上げ
-                j += 2;
-            }
-        }
-    }
-}
-
-// ボタンクリックを監視し、クリック時の動作を決める
-function buttonBehavior(popup, button_num, word_list) {
-    for (let i=0; i<button_num; i++) {
-        let button = document.getElementById(`easy-term-auto-${i}`);
-        button.addEventListener('click', function (event) {
-            if (popup.style.display === 'none') {
-                popup.style.display = 'block';
-                for (let j=0; j<word_list.length; j++){
-                    if (button.name === word_list[j]['word']) {
-                        popup.setAttribute('name', word_list[j]['id']);
-                        document.getElementById('easy-term-header-text').innerText = word_list[j]['word'];
-                        document.getElementById('easy-term-short-description').innerText = word_list[j]['short_description'];
-                    }
-                }
-                height = popupAllDesc.clientHeight + popupHeader.clientHeight;
-                console.log(height);
-                popup.style.height = height + "px";
-            } else {
-                popup.style.display = 'none';
-                document.getElementById('easy-term-detailed-description').innerText = "";
-            }
-        });
-    }
-}
-
-// ~~~~~~~~~~~~~~関数定義 終~~~~~~~~~~~~~~ //
-
-
 // ~~~~~~~~~~~~~~実際の処理~~~~~~~~~~~~~~ //
 // chromeのlocalstorageから値を取得
 chrome.storage.local.get('slammyIsUse', function(event){
@@ -229,7 +138,7 @@ fetch(apiUrl + "words",{method: "GET"})
     // ボタン押下時の挙動を設定する
     height = popupHeader.clientHeight + popupShortDesc.clientHeight + popupDescBtn.clientHeight;
     console.log(height);
-    buttonBehavior(popup, buttonNum, json)
+    buttonBehavior(popup, buttonNum, json, height)
     /* x(cross)ボタン押下時の挙動 */
     popupCross.addEventListener('click', function (event) {
         popup.style.display = 'none';
---
+++ src/functions.js
@@ -0,0 +1,84 @@
+function buttonBehavior(popup, button_num, word_list, height) {
+    for (let i=0; i<button_num; i++) {
+        let button = document.getElementById(`easy-term-auto-${i}`);
+        button.addEventListener('click', function (event) {
+            if (popup.style.display === 'none') {
+                popup.style.display = 'block';
+                for (let j=0; j<word_list.length; j++){
+                    if (button.name === word_list[j]['word']) {
+                        popup.setAttribute('name', word_list[j]['id']);
+                        document.getElementById('easy-term-header-text').innerText = word_list[j]['word'];
+                        document.getElementById('easy-term-short-description').innerText = word_list[j]['short_description'];
+                    }
+                }
+                height = popupAllDesc.clientHeight + popupHeader.clientHeight;
+                popup.style.height = height + "px";
+            } else {
+                popup.style.display = 'none';
+                document.getElementById('easy-term-detailed-description').innerText = "";
+            }
+        });
+    }
+}
+
+
+function setButtonsInPage(paragraphs, word) {
+    // paragraphsに入っているノードごとに処理
+    for (i=0; i<paragraphs.length; i++) {
+        // paragraphはparagraphsの上からi番目の要素
+        paragraph = paragraphs[i];
+
+        // paragraphの子ノードを一つずつ処理
+        for (let j=0; j<paragraph.childNodes.length; j++) {
+            // childはparagraphの子ノード
+            child = paragraph.childNodes[j];
+            // textはparagraphのテキスト
+            text = child.textContent;
+            // whereは単語がtextの中で出てくる位置
+            where = text.indexOf(word);
+            // newNodesを空にしておく
+            newNodes = [];
+            if(where !== -1) {
+                // textに用語が含まれていれば新しいテキストノードとaタグの子要素を作成する
+                // textを用語以前と用語より後に分割し、それぞれnewTextとnewText2に代入する
+                newText = text.substr(0, where+word.length);
+                newText2 = text.substr(where+word.length);
+                // newText2の1文字目が日本語でない場合、ここより下の行は実行しない
+                let first_char = newText2.charAt(1);
+                let last_char = newText.charAt(newText.length);
+                if (!ja2Bit(first_char) && !ja2Bit(last_char)) {
+                    continue;
+                }
+                // aタグのノードを作成する
+                a = document.createElement('a');
+                a.text = '解説';
+                a.id = `easy-term-auto-${buttonNum}`;
+                a.name = word;
+                a.style.fontSize = '30%';
+                // 新しいノードをnewNodesにまとめておく
+                newNodes.push(document.createTextNode(newText))
+                newNodes.push(a);
+                newNodes.push(document.createTextNode(newText2));
+
+                // paragraphの新しいchildと置き換える古いchildを削除
+                paragraph.removeChild(child);
+
+                // paragraphに新しいテキストノードを挿入する
+                for (let x=newNodes.length-1; x>=0; x--) {
+                    paragraph.insertBefore(newNodes[x], paragraph.childNodes[j]);
+                }
+
+                // HTMLタグのid用の変数を数え上げ
+                buttonNum++;
+                // 新しい要素が2つ追加されているので、jを2繰り上げ
+                j += 2;
+            }
+        }
+    }
+}
+
+
+function ja2Bit ( str ) {
+  return ( str.match(/^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/) )? true : false;
+}
+
