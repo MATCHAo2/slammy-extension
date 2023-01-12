fbdb144413b2535e4456b59381b59323432411bc
Naoto Matsumoto <s1F102001243@iniad.org>
2023-01-12 20:33:38 +0900

    popupの閉じかたのパラメータを設定できるようにしました

--- src/config.js
+++ src/config.js
@@ -2,8 +2,10 @@
 // 保存ボタンを押されたときにchromeのlocal storageに設定項目を保存
 function save_configs() {
     let isUse = document.getElementById('easy-term-config-is-use').checked;
+    let closeOption = document.getElementById('easy-term-config-close-option');
     chrome.storage.local.set({
-        slammyIsUse: isUse
+        slammyIsUse: isUse,
+        closeOption: closeOption
     }, function() {
         let message = document.getElementById('easy-term-config-message');
         message.innerText = '保存しました！再読み込みで反映されます！';
@@ -14,6 +16,9 @@ function save_configs() {
 function restore_configs() {
     chrome.storage.local.get("slammyIsUse", function(value) {
         document.getElementById('easy-term-config-is-use').checked = value.slammyIsUse;
+    });
+    chrome.storage.local.get("closeOption", function(value) {
+        document.getElementById('easy-term-config-close-option').checked = value.closeOption;
     });
 }
 
